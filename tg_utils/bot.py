37fe0ab62157f38a6b6b1b42a6c75397877543ce
dongg <dongguho@gmail.com>
2018-03-03 21:46:14 +0800

    bug fixed

--- tg_utils/bot.py
+++ tg_utils/bot.py
@@ -170,8 +170,8 @@ class Bot(User):
         return self.post('answerCallbackQuery', ['callback_query_id', 'text', 'show_alert', 'url', 'cache_time'], callback_query_id=callback_query_id, **kwargs)
 
     def editMessageText(self, text, **kwargs):
-        return self.post('editMessageText', ['chat_id', 'message_id', 'inline_message_id', 'text', 'parse_mode', 'disable_web_page_preview', 'reply_markup',
-                         text=text, **kwargs])
+        return self.post('editMessageText', ['chat_id', 'message_id', 'inline_message_id', 'text', 'parse_mode', 'disable_web_page_preview', 'reply_markup'],
+                         text=text, **kwargs)
 
     def editMessageCaption(self, **kwargs):
         return self.post('editMessageCaption', ['chat_id', 'message_id', 'inline_message_id', 'caption', 'parse_mode', 'reply_markup'], **kwargs)
